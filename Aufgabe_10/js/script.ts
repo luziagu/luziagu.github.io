1754a2f912280fb3219ddd9ff2f7ad7834291c57
luziagu <luzia.gunzenhauser@yahoo.de>
2020-01-14 22:55:40 +0100

    Aufgabe 10

--- Aufgabe_10/js/script.ts
+++ Aufgabe_10/js/script.ts
@@ -142,7 +142,7 @@ function updateCounter(): void {
             toDoDone++;
         } else {
             toDoOpen++; 
-        };
+        }
         
     }
 
@@ -249,3 +249,51 @@ function deleteTodo(index: number): void {
      */
     drawListToDOM();
 }
+
+declare var Artyom: any;
+
+window.addEventListener("load", function(): void {
+    const artyom: any = new Artyom();
+    
+    artyom.addCommands({ 
+        indexes: ["Erstelle Aufgabe *"], 
+        smart: true,
+        action: function(i: any, wildcard: string): void {
+            alert("Neue Aufgabe: " + wildcard);
+
+            toDos.push( 
+                { 
+                    todosText: (wildcard),
+                    todoChecked: false
+    
+                }
+
+                 
+            );
+            console.log("ich wurde aufgerufen"); 
+
+            drawListToDOM();
+        }
+    });
+    
+    function startContinuousArtyom(): void {
+        artyom.fatality();
+    
+        setTimeout(
+            function(): void {
+                artyom.initialize({
+                    lang: "de-DE", 
+                    continuous: true,
+                    listen: true, 
+                    interimResults: true,
+                    debug: true
+                }).then(function(): void {
+                    console.log("Ready!");
+                });
+            }, 
+            250);
+    }
+    
+    startContinuousArtyom();
+    
+});
